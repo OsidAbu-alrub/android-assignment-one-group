91f0818fdb97ca8ab7eae0b86cd32cf96d6a8541
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-05-15 17:52:19 +0300

    Add colors

--- app/src/main/java/com/abualrub/androidassignmentonegroup/MainActivity.java
+++ app/src/main/java/com/abualrub/androidassignmentonegroup/MainActivity.java
@@ -11,8 +11,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    private void method(){
-        // this method is on upstream only
-    }
 }
