1e961f36cf5934210bff46fb6b767da1f6475e11
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-05-19 22:32:58 +0300

    Add IURLs

--- app/src/main/java/com/abualrub/androidassignmentonegroup/utils/IURLs.java
+++ app/src/main/java/com/abualrub/androidassignmentonegroup/utils/IURLs.java
@@ -4,24 +4,24 @@ public interface IURLs {
     // GET
     // TAKES: NOTHING
     // RETURNS: ARRAY OF COURSES
-    public static final String URL_COURSES = "http://localhost/android-rest/group-assignment-one/courses.php";
+    public static final String URL_COURSES = "http://10.0.2.2/android-rest/group-assignment-one/courses.php";
     // =================================================================================
     // GET
     // TAKES: studentId
     // RETURNS: Student Object
-    public static final String URL_GET_STUDENT = "http://localhost/android-rest/group-assignment-one/get-student.php";
+    public static final String URL_GET_STUDENT = "http://10.0.2.2/android-rest/group-assignment-one/get-student.php";
     // =================================================================================
     // POST
     // TAKES:
     // 	userName
     // 	password
     // RETURNS: Student Object
-    public static final String URL_LOGIN = "http://localhost/android-rest/group-assignment-one/login.php";
+    public static final String URL_LOGIN = "http://10.0.2.2/android-rest/group-assignment-one/login.php";
     // =================================================================================
     // GET
     // TAKES: studentId
     // RETURNS: ARRAY OF NOTES
-    public static final String URL_NOTES = "http://localhost/android-rest/group-assignment-one/notes.php";
+    public static final String URL_NOTES = "http://10.0.2.2/android-rest/group-assignment-one/notes.php";
     // =================================================================================
     // POST
     // TAKES:
@@ -32,15 +32,15 @@ public interface IURLs {
     // 	email
     // 	phoneNumber
     // RETURNS: Student Object
-    public static final String URL_register = "http://localhost/android-rest/group-assignment-one/register.php";
+    public static final String URL_register = "http://10.0.2.2/android-rest/group-assignment-one/register.php";
     // =================================================================================
     // POST
     // TAKES: Student Object
     // RETURNS: Updated Student Object
-    public static final String URL_UPDATE_STUDENT = "http://localhost/android-rest/group-assignment-one/update-student.php";
+    public static final String URL_UPDATE_STUDENT = "http://10.0.2.2/android-rest/group-assignment-one/update-student.php";
     // =================================================================================
     // GET
     // TAKES: USERNAME
     // RETURNS: USERNAME
-    public static final String URL_VALIDATE_USERNAME = "http://localhost/android-rest/group-assignment-one/validate-username.php";
+    public static final String URL_VALIDATE_USERNAME = "http://10.0.2.2/android-rest/group-assignment-one/validate-username.php";
 }
