e7f7ff8efe25c38fc0c6507defbe8e9119bc8020
Banan Aburameh <banan_majed@hotmail.com>
2021-05-21 18:13:21 +0300

    done

--- app/src/main/java/com/abualrub/androidassignmentonegroup/DetailedActivity.java
+++ app/src/main/java/com/abualrub/androidassignmentonegroup/DetailedActivity.java
@@ -1,14 +1,70 @@
 package com.abualrub.androidassignmentonegroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ScrollView;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetailedActivity extends AppCompatActivity {
-
+        ListView listView;
+        ArrayList<String> ArrayList;
+        Button buttonAdd;
+        EditText editTextWriteNotes;
+        ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed);
+        listView = (ListView)findViewById(R.id.ListViewNotes);
+        buttonAdd = (Button)findViewById(R.id.buttonAdd);
+        editTextWriteNotes = (EditText)findViewById(R.id.editTextNotes);
+
+        ArrayList = new ArrayList<String>();
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,ArrayList);
+
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String notes = editTextWriteNotes.getText().toString();
+
+                ArrayList.add(notes);
+                listView.setAdapter(arrayAdapter);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "lDosBozB9jw";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
     }
 }
