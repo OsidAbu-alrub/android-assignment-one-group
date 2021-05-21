edf01c7777638197913ca7733893c84fab9141a2
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-05-22 01:12:20 +0300

    Add ICategory

---
+++ app/src/main/java/com/abualrub/androidassignmentonegroup/utils/ICategory.java
@@ -0,0 +1,20 @@
+package com.abualrub.androidassignmentonegroup.utils;
+
+import com.abualrub.androidassignmentonegroup.R;
+
+// *********************************
+// MADE BY OSID ABU-ALRUB (1183096)
+// *********************************
+public interface ICategory {
+    public static final String PROGRAMMING = "programming";
+    public static final String LANGUAGES = "languages";
+    public static final String COOKING = "cooking";
+    public static final String SCIENCE = "science";
+    public static final String LIFE = "life";
+
+    public static final int PROGRAMMING_ICON = R.drawable.programming;
+    public static final int LANGUAGE_ICON = R.drawable.languages;
+    public static final int COOKING_ICON = R.drawable.cooking;
+    public static final int SCIENCE_ICON = R.drawable.science;
+    public static final int LIFE_ICON = R.drawable.life;
+}
