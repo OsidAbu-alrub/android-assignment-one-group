1cc91d759ab6d0b9f4a5d3f16421a8d75d566569
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-05-22 05:35:30 +0300

    Save IP in variable

--- app/src/main/java/com/abualrub/androidassignmentonegroup/utils/IURLs.java
+++ app/src/main/java/com/abualrub/androidassignmentonegroup/utils/IURLs.java
@@ -4,22 +4,25 @@ package com.abualrub.androidassignmentonegroup.utils;
 // MADE BY OSID ABU-ALRUB (1183096)
 // *********************************
 public interface IURLs {
+    // this is my laptop's ip address (localhost)
+    public static final String IP_ADDRESS = "98.90.90.110";
+
     // GET
     // TAKES: NOTHING
     // RETURNS: ARRAY OF COURSES
-    public static final String URL_COURSES = "http://98.90.90.110/android-rest/group-assignment-one/courses.php";
+    public static final String URL_COURSES = "http://"+IP_ADDRESS+"/android-rest/group-assignment-one/courses.php";
     // =================================================================================
     // GET
     // TAKES: studentId
     // RETURNS: Student Object
-    public static final String URL_GET_STUDENT = "http://98.90.90.110/android-rest/group-assignment-one/get-student.php";
+    public static final String URL_GET_STUDENT = "http://"+IP_ADDRESS+"/android-rest/group-assignment-one/get-student.php";
     // =================================================================================
     // POST
     // TAKES:
     // 	userName
     // 	password
     // RETURNS: Student Object
-    public static final String URL_LOGIN = "http://98.90.90.110/android-rest/group-assignment-one/login.php";
+    public static final String URL_LOGIN = "http://"+IP_ADDRESS+"/android-rest/group-assignment-one/login.php";
     // =================================================================================
     // GET
     // TAKES:
@@ -27,7 +30,7 @@ public interface IURLs {
     //	courseId
     // RETURNS:
     //  notes array
-    public static final String URL_NOTES = "http://98.90.90.110/android-rest/group-assignment-one/notes.php";
+    public static final String URL_NOTES = "http://"+IP_ADDRESS+"/android-rest/group-assignment-one/notes.php";
     // =================================================================================
     // POST
     // TAKES:
@@ -38,19 +41,19 @@ public interface IURLs {
     // 	email
     // 	phoneNumber
     // RETURNS: Student Object
-    public static final String URL_REGISTER = "http://98.90.90.110/android-rest/group-assignment-one/register.php";
+    public static final String URL_REGISTER = "http://"+IP_ADDRESS+"/android-rest/group-assignment-one/register.php";
     // =================================================================================
     // POST
     // TAKES: Student Object
     // RETURNS: Updated Student Object
-    public static final String URL_UPDATE_STUDENT = "http://98.90.90.110/android-rest/group-assignment-one/update-student.php";
+    public static final String URL_UPDATE_STUDENT = "http://"+IP_ADDRESS+"/android-rest/group-assignment-one/update-student.php";
     // =================================================================================
     // POST
     // TAKES:
-    //	studentId
-    //	courseId
+    //  studentId
+    //  courseId
     //  notes array (strings)
     // RETURNS:
     //  Message object
-    public static final String URL_UPDATE_NOTES = "http://98.90.90.110/android-rest/group-assignment-one/update-notes.php";
+    public static final String URL_ADD_NEW_NOTES = "http://"+IP_ADDRESS+"/android-rest/group-assignment-one/add-new-notes.php";
 }
